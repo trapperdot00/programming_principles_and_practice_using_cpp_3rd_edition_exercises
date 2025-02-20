3f7c7af86a6beac9dfa02fa4a75f70a99664faa9
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2025-02-21 00:32:33 +0100

    finished exercise10 in chapter10

--- Chapter10/exercises/10/main.cpp
+++ Chapter10/exercises/10/main.cpp
@@ -49,14 +49,18 @@ struct Attributes {
     inline static constexpr int big_box_width = normal_box_width + 40;
     inline static constexpr int big_box_height = normal_box_height + 48;
 
+    // Line
+    inline static const Color line_color = Color::black;
+
     // Arrow
-    inline static constexpr int arrowhead_angle = 50;
-    inline static constexpr int arrowhead_height = 10;
+    inline static const Color arrowhead_color = Color::black;
+    inline static constexpr int arrowhead_angle = 25;
+    inline static constexpr int arrowhead_height = 20;
 };
 
 class Handler {
 public:
-    static void handle_titles(Window& win, const std::vector<Text*>& titles) {
+    static void handle_titles(Window& win, std::vector<Text*>& titles) {
         for (Text* p : titles) {
             p->set_font_size(Attributes::title_size);
             p->set_color(Attributes::title_color);
@@ -64,7 +68,7 @@ public:
             win.attach(*p);
         }
     }
-    static void handle_text(Window& win, const std::vector<Text*>& text) {
+    static void handle_text(Window& win, std::vector<Text*>& text) {
         for (Text* p : text) {
             p->set_font_size(Attributes::text_size);
             p->set_color(Attributes::text_color);
@@ -72,7 +76,7 @@ public:
             win.attach(*p);
         }
     }
-    static void handle_comments(Window& win, const std::vector<Text*>& comments) {
+    static void handle_comments(Window& win, std::vector<Text*>& comments) {
         for (Text* p : comments) {
             p->set_font_size(Attributes::comment_size);
             p->set_color(Attributes::comment_color);
@@ -80,9 +84,21 @@ public:
             win.attach(*p);
         }
     }
-    static void handle_boxes(Window& win, const std::vector<Rectangle*>& boxes) {
+    static void handle_boxes(Window& win, std::vector<Rectangle*>& boxes) {
         for (Rectangle* p : boxes) {
             p->set_fill_color(Attributes::box_fill_color);
+            win.attach(*p);
+        }
+    }
+    static void handle_lines(Window& win, std::vector<Line*>& lines) {
+        for (Line* p : lines) {
+            p->set_color(Attributes::line_color);
+            win.attach(*p);
+        }
+    }
+    static void handle_arrowheads(Window& win, std::vector<Polygon*> arrowheads) {
+        for (Polygon* p : arrowheads) {
+            p->set_fill_color(Attributes::arrowhead_color);
             win.attach(*p);
         }
     }
@@ -112,6 +128,10 @@ struct Circle_data {
     double radius;
 };
 
+std::ostream& operator<<(std::ostream& os, const Circle_data& p) {
+    return os << "(x-" << -p.center.x << ")^2 + (y-" << -p.center.y << ")^2 = " << std::pow(p.radius, 2);
+}
+
 std::pair<int, int> quadratic_formula(double a, double b, double c) {
     int x1 = (-b + std::sqrt(std::pow(b, 2) - 4 * a * c)) / (2 * a);
     int x2 = (-b - std::sqrt(std::pow(b, 2) - 4 * a * c)) / (2 * a);
@@ -127,7 +147,7 @@ double get_slope(const Point& p1, const Point& p2) {
         throw std::runtime_error{"points don't differ"};
     int delta_x = p2.x - p1.x;
     int delta_y = p2.y - p1.y;
-    double slope = delta_y / delta_x;
+    double slope = double(delta_y) / delta_x;
     return slope;
 }
 
@@ -139,6 +159,15 @@ double get_slope(const GenericVector& v) {
     }
     double slope = v.delta_y / v.delta_x;
     return slope;
+}
+
+bool vertical(const Point& p1, const Point& p2) {
+    int delta_x = p2.x - p1.x;
+    return delta_x == 0;
+}
+bool upwards(const Point& p1, const Point& p2) {
+    int delta_y = p2.y - p1.y;
+    return delta_y < 0;
 }
 
 int get_y_intercept(double slope, const Point& p) {
@@ -187,20 +216,10 @@ private:
     int y_;
 };
 
-/*
-    mx + b = nx + c
-    mx = nx + c - b
-    mx - nx = c - b
-    x(m-n) = c - b
-    x = (c - b) / (m - n)
-*/
+std::ostream& operator<<(std::ostream& os, const Line_data& l) {
+    return os << "y = " << l.slope() << "x + " << l.y_intercept();
+}
 
-/*
-    mx + b = nx + c
-    mx - nx = c - b
-    x(m-n) = c - b
-    x = (c - b) / (m - n)
-*/
 Point intersection(const Line_data& l1, const Line_data& l2) {
     if (l1.slope() == l2.slope())
         throw 0;
@@ -220,7 +239,35 @@ std::pair<Point, Point> intersection(const Line_data& line, const Circle_data& c
 
     return std::pair{ Point{x1, y1}, Point{x2, y2} };
 }
-// -------------------
+
+Point get_closer(const std::pair<Point, Point>& possible, const Point& p) {
+    if (distance(possible.first, p) < distance(possible.second, p))
+        return possible.first;
+    return possible.second;
+}
+
+double to_radians(int degrees) {
+    return degrees * (M_PI / 180);
+}
+
+std::pair<Point, Point> get_arrowhead_coordinates(const Point& start, const Point& end) {
+    Circle_data circle{end, Attributes::arrowhead_height};
+    double arrow_angle = to_radians(Attributes::arrowhead_angle / 2);
+    if (vertical(start, end)) {
+        int delta_x = std::tan(arrow_angle) * Attributes::arrowhead_height;
+        Point p1{end.x - delta_x, end.y + Attributes::arrowhead_height};
+        Point p2{end.x + delta_x, end.y + Attributes::arrowhead_height};
+        return std::make_pair(p1, p2);
+    }
+    Line_data line{start, end};
+    double slope1 = std::tan(std::atan(line.slope()) + std::tan(arrow_angle));
+    double slope2 = std::tan(std::atan(line.slope()) - std::tan(arrow_angle));
+    Line_data arrow_line1{slope1, end};
+    Line_data arrow_line2{slope2, end};
+    Point p1 = get_closer(intersection(arrow_line1, circle), start);
+    Point p2 = get_closer(intersection(arrow_line2, circle), start);
+    return std::make_pair(p1, p2);
+}
 
 int main(int /*argc*/, char * /*argv*/[])
 {
@@ -440,31 +487,56 @@ int main(int /*argc*/, char * /*argv*/[])
 
     // Lines
     Point graph_top_center{(graph_box_pos.x + graph_box.width()) / 2, graph_box_pos.y};
-    Point point_bottom_center{(point_box_pos.x + point_box.width()) / 2, point_box_pos.y + point_box.height()};
-    Line graph_to_point{graph_top_center, point_bottom_center};
-    Line_data graph_to_point_data{graph_top_center, point_bottom_center};
+    Point point_bottom_center{(point_box_pos.x + 0 + point_box.width()) / 2, point_box_pos.y + point_box.height()};
+    Line line1{graph_top_center, point_bottom_center};
+    Polygon arrowhead1;
+    auto [p1, p2] = get_arrowhead_coordinates(graph_top_center, point_bottom_center);
+    arrowhead1.add(point_bottom_center);
+    arrowhead1.add(p1);
+    arrowhead1.add(p2);
 
-    Polygon graph_to_point_head;
-    graph_to_point_head.add(point_bottom_center);
-    if (std::isfinite(graph_to_point_data.slope())) {
-        Circle_data graph_to_point_helper{point_bottom_center, Attributes::arrowhead_height};
-        Line_data arrow_line1{
-            graph_to_point_data.slope() + Attributes::arrowhead_angle / 2,
-            point_bottom_center
-        };
-        Line_data arrow_line2{
-            graph_to_point_data.slope() - Attributes::arrowhead_angle / 2,
-            point_bottom_center
-        };
-        std::pair<Point, Point> [p1, p2] = intersection(arrow_line1, graph_to_point_helper);
-        auto [p3, p4] = intersection(arrow_line2, graph_to_point_helper);
-        std::cout << p1.x << '\n';
-        graph_to_point_head.add(p1);
-        graph_to_point_head.add(p3);
-    } else {
-        std::cout << "lol" << std::endl;
-    }
-    graph_to_point_head.set_fill_color(Color::black);
+    Point window_left_middle{window_box_pos.x, window_box_pos.y + window_box.height() / 2};
+    Line line2{window_left_middle, point_bottom_center};
+    Polygon arrowhead2;
+    auto [p3, p4] = get_arrowhead_coordinates(window_left_middle, point_bottom_center);
+    arrowhead2.add(point_bottom_center);
+    arrowhead2.add(p3);
+    arrowhead2.add(p4);
+
+    Point gui_left_middle{gui_box_pos.x, gui_box_pos.y + gui_box.height() / 2};
+    Point window_right_middle{window_box_pos.x + window_box.width(), window_box_pos.y + window_box.height() / 2};
+    Line line3{gui_left_middle, window_right_middle};
+    Polygon arrowhead3;
+    auto [p5, p6] = get_arrowhead_coordinates(gui_left_middle, window_right_middle);
+    arrowhead3.add(window_right_middle);
+    arrowhead3.add(p5);
+    arrowhead3.add(p6);
+
+    Point simple_window_right_middle{simple_window_box_pos.x + simple_window_box.width(), simple_window_box_pos.y + simple_window_box.height() / 2};
+    Point gui_bottom_middle{gui_box_pos.x + gui_box.width() / 2, gui_box_pos.y + gui_box.height()};
+    Line line4{simple_window_right_middle, gui_bottom_middle};
+    Polygon arrowhead4;
+    auto [p7, p8] = get_arrowhead_coordinates(simple_window_right_middle, gui_bottom_middle);
+    arrowhead4.add(gui_bottom_middle);
+    arrowhead4.add(p7);
+    arrowhead4.add(p8);
+
+    Point ch10_top_middle{ch10_box_pos.x + ch10_box.width() / 2, ch10_box_pos.y};
+    Point simple_window_bottom_middle{simple_window_box_pos.x + simple_window_box.width() / 2, simple_window_box_pos.y + simple_window_box.height()};
+    Line line5{ch10_top_middle, simple_window_bottom_middle};
+    Polygon arrowhead5;
+    auto [p9, p10] = get_arrowhead_coordinates(ch10_top_middle, simple_window_bottom_middle);
+    arrowhead5.add(simple_window_bottom_middle);
+    arrowhead5.add(p9);
+    arrowhead5.add(p10);
+
+    Point graph_bottom_middle{graph_box_pos.x + graph_box.width() / 2, graph_box_pos.y + graph_box.height()};
+    Line line6{ch10_top_middle, graph_bottom_middle};
+    Polygon arrowhead6;
+    auto [p11, p12] = get_arrowhead_coordinates(ch10_top_middle, graph_bottom_middle);
+    arrowhead6.add(graph_bottom_middle);
+    arrowhead6.add(p11);
+    arrowhead6.add(p12);
 
     // Pointers to all objects based on their types
     std::vector<Rectangle*> boxes = {
@@ -484,6 +556,13 @@ int main(int /*argc*/, char * /*argv*/[])
         &window_text_3, &gui_text_2, &gui_text_3, &simple_window_text_2,
         &ch10_text_1
     };
+    std::vector<Line*> lines = {
+        &line1, &line2, &line3, &line4, &line5, &line6
+    };
+    std::vector<Polygon*> arrowheads = {
+        &arrowhead1, &arrowhead2, &arrowhead3,
+        &arrowhead4, &arrowhead5, &arrowhead6
+    };
 
 
     // Format and attach objects
@@ -491,8 +570,8 @@ int main(int /*argc*/, char * /*argv*/[])
     Handler::handle_titles(win, titles);
     Handler::handle_comments(win, comments);
     Handler::handle_text(win, text);
+    Handler::handle_lines(win, lines);
+    Handler::handle_arrowheads(win, arrowheads);
 
-    win.attach(graph_to_point);
-    win.attach(graph_to_point_head);
     win.wait_for_button();
 }
