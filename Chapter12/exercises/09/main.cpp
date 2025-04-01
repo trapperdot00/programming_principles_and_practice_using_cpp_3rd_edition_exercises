1540369f02f1f203d974a51dfab4cefd9014ce72
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2025-04-01 19:14:27 +0200

    finished exercise09 for chapter12

--- Chapter12/exercises/09/main.cpp
+++ Chapter12/exercises/09/main.cpp
@@ -1,33 +1,43 @@
 #include "PPP/Simple_window.h"
 #include "PPP/Graph.h"
 
-class Rounded : public Rectangle {
+class Rounded : public Lines {
 public:
-    Rounded(Point p, int w, int h)
-        : Rectangle{p, w, h}
+    Rounded(Point p, int w, int h, int r)
+        : Lines{
+            Point{p.x, p.y + r}, Point{p.x, p.y + h - r},
+            Point{p.x + r, p.y + h}, Point{p.x + w - r, p.y + h},
+            Point{p.x + w, p.y + h - r}, Point{p.x + w, p.y + r},
+            Point{p.x + w - r, p.y}, Point{p.x + r, p.y}
+        }
     {
-        create_corners(w / 4, h / 4);
+        if (std::min(w, h) / 2 < r)
+            throw std::runtime_error{"invalid radius"};
+        create_corners(r);
     }
-    Rounded(Point p1, Point p2)
-        : Rectangle{p1, p2}
-    {
-        create_corners(p2.x - p1.x, p2.y - p1.y);
-    }
+    Rounded(Point p1, Point p2, int r)
+        : Rounded(p1, p2.x - p1.x, p2.y - p1.y, r)
+    {}
+
     void draw_specifics(Painter& painter) const override {
-        Rectangle::draw_specifics(painter);
+        Lines::draw_specifics(painter);
         for (Arc* p : c)
             p->draw_specifics(painter);
     }
+
 private:
     std::unique_ptr<Arc> create_corner
-    (Point p, int w, int h, double st, double rot) const {
-        return std::make_unique<Arc>(p, w, h, st, rot);
+    (Point p, int r, double st, double rot) const {
+        return std::make_unique<Arc>(p, r, r, st, rot);
     }
-    void create_corners(int w, int h) {
+
+    void create_corners(int r) {
         for (int i = 0; i < 4; ++i) {
-            double angle = M_PI / 2 * i;
-            Point p{100, 100};
-            c.push_back(create_corner(p, w, h, angle, 90));
+            int curr = (i * 2) % number_of_points();
+            int prev = curr > 0 ? curr - 1 : number_of_points() - 1;
+            if (i && i % 2)
+                swap(prev, curr);
+            c.push_back(create_corner(Point{point(prev).x, point(curr).y}, r, (i + 1) * 90, 90));
         }
     }
     Vector_ref<Arc> c;
@@ -49,8 +59,14 @@ int main(int /*argc*/, char * /*argv*/[])
     const std::string label = "Rounded";
     Simple_window win{Point{win_x, win_y}, win_width, win_height, label};
 
-    Rounded r{Point{100, 100}, 200, 200};
-    win.attach(r);
+    constexpr Point center{win_width / 2, win_height / 2};
+    constexpr int r_width = 200;
+    constexpr int r_height = 200;
+    Rounded r1{Point{center.x - r_width / 2, center.y - r_height / 2}, r_width, r_height, 20};
+    win.attach(r1);
+
+    Rounded r2{Point{0,0}, Point{win_width, win_height}, 50};
+    win.attach(r2);
 
     win.wait_for_button();
 }
